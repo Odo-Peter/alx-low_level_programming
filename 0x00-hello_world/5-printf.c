1e0ed30f02a0642d2e7f846ecc52951aa8fa592d
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-16 11:09:35 -0800

    commit message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - prints the string given
  * Return: 0 for success
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
