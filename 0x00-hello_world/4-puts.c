cdf9bc549caee85ae2bec49fa2a887149f5b6e28
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-16 10:44:58 -0800

    commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
  * main - prints the char str
  * Return: The str
  */
+
 int main(void)
 {
-	char str[] = ("\"Programming is like building a multilingual puzzle");
+	char str[]= "\"Programming is like building a multilingual puzzle";
 	puts(str);
 	return (0);
 }
