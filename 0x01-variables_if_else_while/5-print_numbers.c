9796d9ae95eef05d37bc139a45ae3a47f0db5e54
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-17 13:01:08 -0800

    committed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints out numbers less than 10
+ * Return: 0 for completion
+*/
+int main(void)
+{
+	int i;
+
+	/*The for loop for the numbers being printed*/
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return(0);
+}
