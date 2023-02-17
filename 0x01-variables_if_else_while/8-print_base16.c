9eb1b466f7a8ae42ce352d5659869a2268d7623a
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-17 13:30:30 -0800

    committed

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the hexadecimals values
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char c;
+
+	/*first for loop for 0-9*/
+	for (c = 0; c < 58; c++)
+	{
+		putchar(c);
+	}
+	for (c = 97; c <= 102; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
