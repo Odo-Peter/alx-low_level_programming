02c715f35c1dbd359dc216147aadd8cbe10253f7
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-17 12:55:49 -0800

    committed

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints out the chars except q and e
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char a;
+
+	/*The for loop for the alphabets*/
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'q' || a !='e')
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
