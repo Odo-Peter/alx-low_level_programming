974406ff8c2f4403948d6ac04e6acad37c6b0122
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-17 13:43:45 -0800

    committed

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints single digits
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int i;
+
+	/*The for looping proper*/
+	for (i = 48, i < 58; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(" ");
+	}
+	putchar('\n');
+	return (0);
+}
