3a8bb075122358f2ea5e85efb76cc8d85ad99767
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-17 13:12:28 -0800

    committed

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the ints values 1 - 10
+ * Return: 0 for completion
+ */
+
+int main(void)
+{
+	int i;
+
+	/*Performing the for loop*/
+	for (i = 65; i < 75; i++)
+	{
+		putchar(i);
+	}
+	putcar('\n');
+	return (0);
+}
