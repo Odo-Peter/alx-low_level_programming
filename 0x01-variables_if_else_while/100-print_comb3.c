662fc291985a480e4e69d3996e4113f649bbe289
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-19 05:00:22 -0800

    committed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 	int i, j;
 
 	/*the for loop to print in the chars*/
-	for (i = 1; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = i + 1; j < 9; j++)
 		{
-			if(i != j)
+			if (i != j)
 			{
 				putchar(i + '0');
-				putchar(j +'0');
-				if(i + j < 17)
+				putchar(j + '0');
+				if (i + j < 17)
 				{
 					putchar(',');
 					putchar(' ');
