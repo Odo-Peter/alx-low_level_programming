fc785f213f51c1dcbfb6ce94f9e6447fec898af0
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-21 10:04:06 -0800

    usual commits

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _islower - prints out checks for chars that lowercase
+ * Return: 1 for a lowercase char and 0 for otherwise
+ */
+
+int _islower(void)
+{
+	int i;
+	/*The loop to check chars*/
+	for (i = 65; i <= 122; i++)
+	{
+		if (_putchar(i) > _putchar(96))
+		{
+			return (1);
+		} else if (_putchar(i) < _putchar(91))
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
