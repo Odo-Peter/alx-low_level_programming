723139444a1a86e70fcbe764168fd43a7645539d
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-21 11:49:44 -0800

    usual commits

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of an int
+ * @i: the params passed to the function
+ * Return: 0 always
+ */
+
+int print_last_digit(int i)
+{
+	int l;
+	/*Some modulus computation*/
+	l = i % 10;
+	return (l);
+}
