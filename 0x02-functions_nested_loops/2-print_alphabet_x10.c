b4e7f37b80b6431f9f3560a518e192f103737ee7
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-21 05:07:29 -0800

    usual commits

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the chars of the alphabets 10x
+ * Return: 0 always
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+	/*The for and while loops*/
+	while (i < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
