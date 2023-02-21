14f1b751343b6a0480af8c3a10b55edaefa18d8c
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-21 04:53:35 -0800

    usual commits

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,22 @@
  * main - prints the letters of the alphabets
  * Return: 0 for success
  */
+void print_alphabet(void);
 
 int main(void)
 {
 	print_alphabet();
 	return (0);
+}
+
+void print_alphabet(void)
+{
+	char i;
+	
+	/*The loop proper*/
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
