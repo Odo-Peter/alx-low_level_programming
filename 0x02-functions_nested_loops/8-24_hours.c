1f82809712e6ec3cbc8fef4215d652655691d818
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-21 18:52:34 -0800

    usual commits

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * jack_bauer - prints out every minute in a day
+ * Return: no return value for a void function
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	/*The for loop goes below here*/
+	for (i = 0; i < 1440; i++)
+	{
+		int h = i / 60;
+		int m = 1 % 60;
+		_putchar(h);
+		_putchar(':');
+		_putchar(m);
+	}
+}
