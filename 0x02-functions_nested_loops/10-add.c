3c8262ab84d3dc209036de82d34a84f399440568
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-21 20:06:10 -0800

    usual commits

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - prints out the addition of two ints
+ * @a: first params
+ * @b: second params
+ * Return: 0 always
+ */
+
+int add(int a, int b)
+{
+	int c;
+	/*Some computations*/
+	c = a + b;
+	return (c);
+}
