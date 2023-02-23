63bb9483500d214c03f844ed98e62f447f65c48c
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-23 01:04:47 -0800

    usual commits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two ints
+ * @a: the first params
+ *@b: the second params
+ * Return: 0 always for success
+ */
+
+int mul(int a, int b)
+{
+	int c;
+	/*Some computations*/
+	c = a * b;
+	return (c);
+}
