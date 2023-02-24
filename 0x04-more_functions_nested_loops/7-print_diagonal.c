b5b36a69b13bb922deb915d68db39ce2fb595e03
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-23 17:08:51 -0800

    usual commits

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,10 +17,9 @@ void print_diagonal(int n)
 		{
 			_putchar('\\');
 			_putchar('\n');
-			_putchar(' ');
-			for (j = 0; j < i; j++)
+			for (j = 0; j <= i; j++)
 			{
-				_putchar(' ');
+		       		_putchar(' ');
 			}
 		}
 		_putchar('\n');
