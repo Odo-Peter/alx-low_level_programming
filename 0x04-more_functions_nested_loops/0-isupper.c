e6f4095eeeea54c3ba7b48731d82aa4f74c625aa
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-23 17:26:23 -0800

    usual commits

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 /**
  * _isupper - checks if a value is uppercased
  * @c: the params of the function
- * Return: 0 always
+ * Return: 0 or 1 on completion
  */
 
 int _isupper(char c)
@@ -10,6 +10,10 @@ int _isupper(char c)
 	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 	return (0);
 }
