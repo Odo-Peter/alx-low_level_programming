3dc91556adcfdf46120be3513a53cbdf439a5f4d
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-27 20:37:31 -0800

    usual commits

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * void puts2 - prints a str without the first two letters
+ * @str: the params of the function
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	char *pt = str;
+	/*The loop*/
+	while (*pt)
+	{
+		_putchar(*pt);
+		pt += 2;
+	}
+	_putchar('\n');
+}
