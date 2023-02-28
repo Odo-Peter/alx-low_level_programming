b408012cd796a8dc9cfd0b6a6d822403fa36ee6f
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-27 17:25:02 -0800

    usual commits

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of two ints
+ * @a: the pointer to the first int params
+ * @b: the pointer to the second int params
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	a = &b;
+	b = &a;
+}
