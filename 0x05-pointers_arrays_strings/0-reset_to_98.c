9707fb12cf493bcdb63f9644dac78cd1ec3abdbf
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-27 17:18:36 -0800

    usual commits

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * void reset_to_98 - sets the value of an int to 98
+ * @n: the pointer of the function's params
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
