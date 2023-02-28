52536f4855e045e1c626d443e39b2b1f10e6e5eb
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-02-27 18:16:33 -0800

    usual commits

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - prints out the length of a string
+ * @s: the pointer to the char params
+ * Return: the length of the str
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+	/*some computations below*/
+	char *t = s;
+	while (*t != '\0')
+	{
+		l++;
+		t++;
+	}
+	return (l);
+}
