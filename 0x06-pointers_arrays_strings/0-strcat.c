2ffb021194dacd349f2f16160fcc45e5eabd0b53
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-01 16:00:15 -0800

    usual commits

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - concats two strings
+ * @dest: the first params
+ * @src: the second params
+ * Return: returns a pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	while (*dest)
+	{
+		dest++;
+	}
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (*dest);
+}
