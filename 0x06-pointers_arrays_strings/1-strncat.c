5d4d914c360e7049bbe8bcc6666138ebaa3a4c32
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-01 16:20:33 -0800

    usual commits

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: the first params
+ * @src: the second params
+ * @n: the third params
+ * Return: a char pointer
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	while (*dest)
+	{
+		dest++;
+	}
+	n = 0;
+	/*assigning a value to the n variable*/
+	while (*src && i < n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+	}
+	*dest - '\0';
+	return (dest);
+}
