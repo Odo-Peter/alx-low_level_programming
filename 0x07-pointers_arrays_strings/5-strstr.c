db4e06361ac2a1c50638690239e26b125ca86cab
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-07 14:51:36 -0800

    usual commits

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: first params
+ * @needle: second params
+ * Return: a pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h && *n && h == n)
+		{
+			h++;
+			n++;
+		}
+		if (!*n)
+			return ((char *)haystack);
+		haystack++;
+	}
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_memcpy(void *dest, void *src, int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif
