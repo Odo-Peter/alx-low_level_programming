dc8beac8245f5d62c5d7ce181c1c6fa7a90d4869
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-07 15:25:27 -0800

    usual commits

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,10 +13,10 @@ void print_chessboard(char *a[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			printf("%c", a[i][j]);
+			_putchar(a[i][j] + '0');
 			if (j == 7)
 			{
-				printf('\n');
+				_putchar('\n');
 			}
 		}
 	}
