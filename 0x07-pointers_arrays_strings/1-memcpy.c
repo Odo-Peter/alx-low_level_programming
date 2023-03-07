ccaa5af58d574678db790a7f73c7d29d2ced9390
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-07 04:11:18 -0800

    usual commits

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: destination to be copied to
+ * @src: source to copy from
+ * @n: amount of bytes to be copied
+ * Return: a pointer to the dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
