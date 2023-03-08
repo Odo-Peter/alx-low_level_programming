f9aa9a2918e323108b66efccbd78f2e20fb9953d
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-07 16:12:06 -0800

    usual commits

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_chessboard - prints the chessboard
  * @a: the array of values
@@ -7,23 +8,15 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j, row_start;
+	char *c = a[0];
+	int i, j;
 
-	row_start = 1;
-	for (i = 0; i < 8; i++) {
-		for (j = 0; j < 8; j++) {
-			if (row_start == 1) {
-				a[i][j] = 1;
-			} else {
-				a[i][j] = 0;
-			}
-			row_start *= -1;
-		}
-		row_start *= -1;
-	}
-	for (i = 0; i < 8; i++) {
-		for (j = 0; j < 8; j++) {
-			printf("%d ", *(a[i] + j));
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c ", *c);
+			c++;
 		}
 		printf("\n");
 	}
