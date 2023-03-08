6b99cadf0a7492397087700bdfe7fa68886b24e6
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-07 18:00:33 -0800

    usual commits

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - sets the value to a pointer
+ * @s: first params
+ * @to: second params
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	s = to;
+}
