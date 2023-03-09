27d74d593721534e16e4088b10a640b555a8c748
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-09 14:49:44 -0800

    usual commits

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
- * _sqrt_recursion - returns the natural sqrt of a number
- * @n: the number params
+ * sqrt_helper - returns the natural sqrt of a number
+ * @num: the number params
+ * @low: the second params
+ * @high: the third params
  * Return: 0 for success
  */
 
 int sqrt_helper(int num, int low, int high)
 {
 	int mid, square;
+
 	if (low > high)
 	{
 		return (high);
@@ -20,11 +23,15 @@ int sqrt_helper(int num, int low, int high)
 	} else if (square > num)
 	{
 		return (sqrt_helper(num, low, mid - 1));
-	} else
-	{
-		return (sqrt_helper(num, mid + 1, high));
 	}
+	return (sqrt_helper(num, mid + 1, high));
 }
+
+/**
+ * _sqrt_recursion - returns the sqrt
+ * @n: the num params
+ * Return: 0 for success
+ */
 
 int _sqrt_recursion(int n)
 {
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,22 +1,25 @@
 #include "main.h"
 /**
- * is_prime_number - returns 1 if it is a prime number
+ * is_prime_recursive - returns 1 if it is a prime number
  * @n: the input params
+ * @i: the second params
  * Return: 0 for success
  */
 
 int is_prime_recursive(int n, int i)
 {
 	if (i == 1)
-		return 1;
-	else
-	{
+		return (1);
 		if (n % i == 0)
-			return 0;
-		else
-			return is_prime_recursive(n, i - 1);
-	}
+			return (0);
+		return (is_prime_recursive(n, i - 1));
 }
+
+/**
+ * is_prime_number - returns a prime number
+ * @n: input params
+ * Return: 0 for success
+ */
 
 int is_prime_number(int n)
 {
