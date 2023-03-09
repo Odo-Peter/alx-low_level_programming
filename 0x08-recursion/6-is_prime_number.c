3cb6455c53d788ef5d67e5d1a45e1ea5442b02a2
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-09 14:53:44 -0800

    usual commits

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,10 +9,14 @@
 int is_prime_recursive(int n, int i)
 {
 	if (i == 1)
+	{
 		return (1);
-		if (n % i == 0)
-			return (0);
-		return (is_prime_recursive(n, i - 1));
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (is_prime_recursive(n, i - 1));
 }
 
 /**
