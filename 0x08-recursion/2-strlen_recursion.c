99bf9955f29f1b1f39ac76d3e44ef49478aa62ed
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-08 18:18:19 -0800

    usual commits

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,13 +7,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		s++;
-		len++;
-		_strlen_recursion(s);
+		return (0);
 	}
-	return (len);
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
 }
