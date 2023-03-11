f8471064928c8f230bc667e3e75ecaa8ec23d3f1
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-11 14:24:22 -0800

    usual commits

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the number of args
+ * @argc: the args count
+ * @argv: the array of args string
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argv[0])
+	{
+		printf("%d\n", argc);
+	}
+	return (0);
+}
