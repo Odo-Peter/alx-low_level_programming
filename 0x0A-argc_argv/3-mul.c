37ef1c6a02b401d1f626057d30e5db001f4167d4
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-11 14:58:48 -0800

    usual commits

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,16 @@ int main(int argc, char *argv[])
 {
 	int mul = 0;
 
-	if (argc > 1)
+	if (argc - 1 > 1)
 	{
 		mul = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", mul);
 		return (0);
+	}
+	else if (argc - 1 == 1)
+	{
+		printf("Error\n");
+		return (1);
 	}
 	printf("Error\n");
 	return (1);
