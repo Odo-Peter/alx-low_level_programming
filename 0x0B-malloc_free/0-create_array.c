b734dce8d341741e479266ada04192af605e0509
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-13 16:05:07 -0700

    usual commits

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,12 +13,12 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	arr = (char *)malloc(size * sizeof(char));
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	else if (size > 0)
+	arr = (char *)malloc(size * sizeof(char));
+	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
@@ -26,5 +26,4 @@ char *create_array(unsigned int size, char c)
 		}
 	}
 	return (arr);
-	free(arr);
 }
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - returns ptr to a duplicated string
+ * @str: the string params
+ * Return: ptr to str or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *new_str;
+	size_t len = strlen(str);
+	size_t i;
+
+	if (str == NULL)
+		return (NULL);
+	new_str = (char *)malloc(sizeof(str) * sizeof(int));
+	for (i = 0; i < len + 1; i++)
+	{
+		new_str[i] = str[i];
+	}
+	new_str[len] = '\0';
+	return (new_str);
+	free(new_str);
+}
