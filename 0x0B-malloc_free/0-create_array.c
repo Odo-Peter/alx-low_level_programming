2aaa03f8179ae6ce9e228faa6992f99740db1585
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-13 14:19:09 -0700

    usual commits

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,10 +13,13 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	arr = (char*)malloc(size * sizeof(char));
+	arr = (char *)malloc(size * sizeof(char));
+	if (size == 0)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
+	free(arr);
 	return (arr);
 }
