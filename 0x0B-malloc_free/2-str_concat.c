3b6692c5b47fbf1ab53b5fe74c0544139e520901
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-14 17:40:52 -0700

    usual commits

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,20 +29,18 @@ char *str_concat(char *s1, char *s2)
 	size_t i, j;
 
 	c_str = malloc((t_len) * sizeof(char) + 1);
-	if (s1 != NULL && s2 != NULL)
+	for (i = 0; i < len1; i++)
 	{
-		for (i = 0; i < len1; i++)
+		c_str[i] = s1[i];
+	}
+	if (strlen(c_str) == len1)
+	{
+		for (j = 0; j < len2 + 1; j++)
 		{
-			c_str[i] = s1[i];
+			c_str[len1 + j] = s2[j];
 		}
-		if (strlen(c_str) == len1)
-		{
-			for (j = 0; j < len2 + 1; j++)
-			{
-				c_str[len1 + j] = s2[j];
-			}
-		}
-		c_str[t_len] = '\0';
+	}
+	c_str[t_len] = '\0';
 	}
 	if (s2 == NULL || s1 == NULL)
 	{
