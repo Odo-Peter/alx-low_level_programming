1a459d88664914dfc3426e1ea8153f226162c6cc
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-03-20 16:46:50 -0700

    tasks solved

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - a structure for a dog
+ * @name: the dog's name
+ * @age: the age of the dog in floats
+ * @owner: owner of the dog
+ */
 
 struct dog
 {
