a5721677e603443df6bd210a1f0a06e301c9f5f0
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-04-03 11:38:33 -0700

    usual commits

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "list.h"
+
+/**
+ * print_listint - prints all the elements of a list
+ * @h: the head of list params
+ * Retrun: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t num_of_nodes;
+
+	num_of_nodes = 0;
+	while(h != NULL)
+	{
+		printf("%d\n", h -> n);
+		h = h -> next;
+		num_of_nodes++;
+	}
+
+	return (num_of_nodes);
+}
