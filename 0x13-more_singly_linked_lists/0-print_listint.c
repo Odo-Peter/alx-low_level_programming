b557e44f6248bf9282fd1da09c632b0329b7bdf5
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-04-03 11:59:37 -0700

    tasks done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 /**
  * print_listint - prints all the elements of a list
  * @h: the head of list params
- * Retrun: number of nodes
+ * Return: number of nodes
  */
 
 size_t print_listint(const listint_t *h)
