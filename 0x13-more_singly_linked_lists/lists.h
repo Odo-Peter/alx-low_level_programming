f88375f8751b11444fa0ae5d52ed4701a271ab68
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-04-03 11:47:08 -0700

    tasks done

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,18 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - the singly linked list
+ * @next: points to the next node in the list
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 #endif
