b2cd15c6fd0f4726ed8d59821cb0cf1a74f822ba
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-04-03 11:58:41 -0700

    tasks done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,10 +11,10 @@ size_t print_listint(const listint_t *h)
 	size_t num_of_nodes;
 
 	num_of_nodes = 0;
-	while(h != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", h -> n);
-		h = h -> next;
+		printf("%d\n", h->n);
+		h = h->next;
 		num_of_nodes++;
 	}
 
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -12,7 +12,7 @@ size_t listint_len(const listint_t *h)
 	nodes = 0;
 	while (h != NULL)
 	{
-		h = h -> next;
+		h = h->next;
 		nodes++;
 	}
 	return (nodes);
