a2b0d53da10cdef1fb80ba43e7a8c33171c30043
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-04-03 11:56:52 -0700

    tasks done

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints the number of elements in a list
+ * @h: the head of the list params
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes;
+
+	nodes = 0;
+	while (h != NULL)
+	{
+		h = h -> next;
+		nodes++;
+	}
+	return (nodes);
+}
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "list.h"
+
+/**
+ * add_nodeint - adds a node at the begining of a list
+ * @head: a pointer to the head of the list
+ * @n: the second params
+ * Return: address of the new element
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new_node -> n = n;
+	new_node -> next = *head;
+	*head = new_node;
+
+	return (*head);
+}
